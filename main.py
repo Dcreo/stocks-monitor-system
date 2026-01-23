95ac4fbe2d201ef90e5367b1275213d6faf0635b
Nazarov Roman <nazarovbusiness@gmail.com>
2026-01-23 14:51:32 +0700

    Stocks list and Stock item endpoints

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ app = FastAPI()
 def index():
     return "Stocks monitor!"
 
+@app.get("/stocks")
+def stocks():
+    return "Public Stocks list"
+
+@app.get("/stocks/{id}")
+def stock(id):
+    return f"Stock with id: {id}"
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True)
