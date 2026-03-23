5a59899109f904f59ab7b25ced92045dc6562f4e
Nazarov Roman <nazarovbusiness@gmail.com>
2026-03-23 12:06:42 +0700

    Sugar for pytest

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-print("**********")
-print(Path(__file__).resolve().parents[1] / ".env")
-
 class Settings(BaseSettings):
     database_url: str
     pythonpath: str
